72cb35a5719f3f10f1c82b92ca8b48475c65e5a3
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2020-10-25 07:57:58 -0500

    middelwares redirect login custom pages errors

--- src/main/java/mx/com/gm/web/HolaSpringController.java
+++ src/main/java/mx/com/gm/web/HolaSpringController.java
@@ -7,6 +7,8 @@ import mx.com.gm.service.PersonaServiceImpl;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -20,9 +22,10 @@ public class HolaSpringController {
     private PersonaServiceImpl personaService;
 
     @GetMapping("/")
-    public String inicio(Model model) {
+    public String inicio(Model model, @AuthenticationPrincipal User user) {
         var personas = personaService.listarPersonas();
         log.info("ejecutando el controlador Spring MVC");
+        log.info("usuario que hizo login" + user);
         model.addAttribute("personas", personas);
         return "index";
     }
